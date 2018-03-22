e62588b5acf87ba0bd53b94e1247e014ca399e19
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2018-03-22 16:36:55 +0600

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/MyLayout.js'
+
+export default () => (
+    <Layout>
+       <p>Sample application on NextJS </p>
+       <span>TODO</span>
+       <ul>
+           <li>try to use ReasonML</li>
+       </ul>
+    </Layout>
+)
